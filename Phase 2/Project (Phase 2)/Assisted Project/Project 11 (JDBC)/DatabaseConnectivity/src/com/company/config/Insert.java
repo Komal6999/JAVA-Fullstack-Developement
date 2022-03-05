8f95f33450cbfb36f3405d398935672012742cb8
unknown <komal.patil0609@gmail.com>
2022-03-05 18:05:00 +0530

    Phase 2

---
+++ Phase 2/Project (Phase 2)/Assisted Project/Project 11 (JDBC)/DatabaseConnectivity/src/com/company/config/Insert.java
@@ -0,0 +1,36 @@
+package com.company.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Insert {
+
+	public static void main(String[] args) {
+		
+		String driver="com.mysql.cj.jdbc.Driver";
+		String url="jdbc:mysql://localhost:3306/company";
+		String username="root";
+		String password="Komal@123";
+		
+		try {
+			//load driver
+			Class.forName(driver);
+			//get connection and store it in con object
+			Connection con= DriverManager.getConnection(url,username,password);
+			//check if the connection is not null
+			if(con!=null)
+			{
+				String sql="insert into student (id,fname,lname,email) values "+"(2,'supriya','mishra','supriya@gmailcom')";
+				Statement stmt=con.createStatement();
+				stmt.execute(sql);
+				
+				System.out.println("Data Inserted Successfully");
+			}
+			else
+				System.out.println("Error while connecting with database");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Phase 2/Session 2/CookieDemo/src/main/java/com/company/cookiedemo/User.java
@@ -0,0 +1,13 @@
+package com.company.cookiedemo;
+
+public class User {
+	private String user;
+	private String password;
+	
+	public User(String user, String password) {
+		super();
+		this.user = user;
+		this.password = password;
+	}
+
+}
